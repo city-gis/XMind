b8e2bec8f0f0a70a612a70932766a70fdc703258
Gvitech-gis <zhaoxiaojie@gvitech.com>
2018-04-08 10:48:39 +0800

    添加系统管理模块（权限）been

---
+++ src/com/usedvehicle/beans/sys_modelmenu.java
@@ -0,0 +1,26 @@
+package com.usedvehicle.beans;
+
+public class sys_modelmenu {
+	private String modelmenuid;
+	private String modelid;
+	private String menuid;
+	public String getModelmenuid() {
+		return modelmenuid;
+	}
+	public void setModelmenuid(String modelmenuid) {
+		this.modelmenuid = modelmenuid;
+	}
+	public String getModelid() {
+		return modelid;
+	}
+	public void setModelid(String modelid) {
+		this.modelid = modelid;
+	}
+	public String getMenuid() {
+		return menuid;
+	}
+	public void setMenuid(String menuid) {
+		this.menuid = menuid;
+	}
+
+}
---
+++ src/com/usedvehicle/beans/sys_userrole.java
@@ -0,0 +1,26 @@
+package com.usedvehicle.beans;
+
+public class sys_userrole {
+	private String userrolelid;
+	private String roleid;
+	private String userid;
+	public String getUserrolelid() {
+		return userrolelid;
+	}
+	public void setUserrolelid(String userrolelid) {
+		this.userrolelid = userrolelid;
+	}
+	public String getRoleid() {
+		return roleid;
+	}
+	public void setRoleid(String roleid) {
+		this.roleid = roleid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+}
